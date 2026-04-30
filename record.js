c499b81b0b5ee1fae100251a62899503f4f3bd14
prakhar9918 <prakhardwivedi@nablasol.com>
2026-04-30 16:29:41 +0530

    build an API and called it in record.js

--- record.js
+++ record.js
@@ -8,23 +8,25 @@
     },
 
     updateSummary2: function () {
-        // this.model.set('summary_2_c', 'Hello World');
-        // if (!this.disposed) {
-        //     this.render();
-        // }
-        let url  =  'https://uselessfacts.jsph.pl/api/v2/facts/random';
-        var self = this;
-        app.api.call('GET', url , null, {
-            success: function (data) {
-                console.log('Data fetched successfully:', data.text);
-                self.model.set('summary_2_c', data.text);
-                if (!self.disposed) {
-                    self.render();
-                }
-            },
-            error: function (error) {
-                console.error('Error fetching data:', error);
-            }
-        });
+    //     this.model.set('summary_2_c', 'Hello World');
+    //     if (!this.disposed) {
+    //       this.render();
+    //    }   
+    
+    var url = app.api.buildURL('demo_announcement/data');
+
+    app.api.call('GET', url, null, {
+    success: (res) => {
+        console.log('API Response : ',res.message);
+        this.model.set('summary_2_c', res.message);
+         if (!this.disposed) {
+          this.render();
+       }  
+    },
+    error: (err) => {
+        console.error(err);
     }
+});
+       
+   }
 })
