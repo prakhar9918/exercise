ee06882e5efb3b69bbb214be8d93f5e69aa4b22c
prakhar9918 <prakhardwivedi@nablasol.com>
2026-04-30 15:41:34 +0530

    Getting data from API but due to security reasons chrome is blokcing the request

--- record.js
+++ record.js
@@ -8,9 +8,23 @@
     },
 
     updateSummary2: function () {
-        this.model.set('summary_2_c', 'Hello World');
-        if (!this.disposed) {
-            this.render();
-        }
+        // this.model.set('summary_2_c', 'Hello World');
+        // if (!this.disposed) {
+        //     this.render();
+        // }
+        let url  =  'https://uselessfacts.jsph.pl/api/v2/facts/random';
+        var self = this;
+        app.api.call('GET', url , null, {
+            success: function (data) {
+                console.log('Data fetched successfully:', data.text);
+                self.model.set('summary_2_c', data.text);
+                if (!self.disposed) {
+                    self.render();
+                }
+            },
+            error: function (error) {
+                console.error('Error fetching data:', error);
+            }
+        });
     }
 })
